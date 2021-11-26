0cf61a7ed3ffd2e87cec806ee812f7bf7980b1b9
Roni44 <69218305+Roni44@users.noreply.github.com>
2021-11-26 15:31:32 +0100

    Add files via upload

---
+++ get_FileFormBlob.ps1
@@ -0,0 +1,21 @@
+﻿#connect to azure
+Connect-AzAccount
+
+#Add module
+Install-Module AzureRM
+
+
+
+$resourceGroup = "PackerAgents"
+ $storageAccountName = "appswin10"
+ $containerName = "appspacker"
+    
+ $storageAccount = Get-AzStorageAccount `
+   -ResourceGroupName $resourceGroup `
+   -Name $storageAccountName
+ $ctx = $storageAccount.Context 
+    
+ Get-AzStorageBlobContent -Blob "7z2106-x64.msi" `
+   -Container $containerName `
+   -Destination "C:\Users\rafal\Downloads\azure\" `
+   -Context $ctx 
